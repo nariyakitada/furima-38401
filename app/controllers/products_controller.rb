e6737aa86b7db025b115220774ae99d20e746bed
nariyakitada <narippe002@icloud.com>
2022-09-27 14:35:15 +0900

    商品情報編集機能

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -23,7 +23,19 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
   
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to product_path
+    else
+      render :edit
+    end
+  end
   
 
   private
